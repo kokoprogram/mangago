5da4948f3828eb96bbecaa4b45b09ea9011df27f
Ronny Wegener <wegener.ronny@gmail.com>
2018-08-05 12:37:46 +0200

    optimized git commits

--- sync.sh
+++ sync.sh
@@ -5,6 +5,13 @@ rm -r -f node_modules
 npm install
 git pull
 node --max-old-space-size=4096 . undefined undefined 2500
-git add .
-git commit -m 'autoupdate'
-git push
+
+if [ $(grep -c '"valid":\s[1-9]' report.json) == 1 ] && [ $(grep -c '"invalid":\s[1-9]' report.json) == 0 ]
+then
+    git add .
+    git commit -m 'autoupdate'
+    git push
+else
+    git clean -f -d
+    git checkout -f
+fi
